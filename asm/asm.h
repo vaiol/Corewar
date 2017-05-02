c17990a0a230ac5856eb200b85477411c164b6fc
Alexander Stepanov <vaiol.ans@gmail.com>
2017-05-02 19:17:45 +0300

    op.h

--- asm/asm.h
+++ asm/asm.h
@@ -2,32 +2,18 @@
 # define ASM_H
 
 # include "../libft/libft.h"
+# include "../op.h"
 # include <fcntl.h>
 
+//typedef struct	s_info
+//{
+//	char 		*name;
+//	char 		*comment;
+//	t_op		*operations;
+//}				t_info;
+//t_info			g_info;
 
-typedef struct	s_argument
-{
-	
-}				t_argument;
-
-typedef struct	s_operation
-{
-	int			opcode;
-	char		*name;
-
-}				t_operation;
-
-typedef struct	s_info
-{
-	char 		*name;
-	char 		*comment;
-	t_operation	*operations;
-
-
-}				t_info;
-t_info			g_info;
-
-char	**read_file(char *file_name);
+char			**read_file(char *file_name);
 
 
 
