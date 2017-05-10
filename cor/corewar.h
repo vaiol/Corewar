73f1a7127a4816c287b1ff4742b4a1047164000e
Ivan Solomakhin <isolomak@e1r4p17.unit.ua>
2017-05-10 18:57:38 +0300

    corewar.02

--- cor/corewar.h
+++ cor/corewar.h
@@ -2,9 +2,27 @@
 #ifndef COREWAR_H
 # define COREWAR_H
 
-#include "../libft/libft.h"
-#include "../libft/get_next_line/get_next_line.h"
-#include "../libft/ft_printf/ft_printf.h"
+# include "../libft/libft.h"
+# include "../libft/get_next_line/get_next_line.h"
+# include "../libft/ft_printf/ft_printf.h"
+
+# include <fcntl.h>
+
+# define MAX_ARGS_NUMBER			4
+# define MAX_PLAYERS				4
+# define MEM_SIZE				(4*1024)
+# define IDX_MOD					(MEM_SIZE / 8)
+# define CHAMP_MAX_SIZE			(MEM_SIZE / 6)
+
+# define PROG_NAME_LENGTH		(128)
+# define COMMENT_LENGTH			(2048)
+
+typedef struct	s_champ
+{
+	size_t		size;
+	char		*code;
+
+}				t_champ;
 
 
 typedef struct	s_fl
@@ -17,12 +35,26 @@ typedef struct	s_fl
 typedef struct	s_data
 {
 	int			index;
+	int			count;
 	t_fl		fl;
+	t_champ		*champs;
 
 }				t_data;
 
+// error_handler
 
 void			error_handler(char *str);
+
+// get_flags
+
 void			check_flags(int argc, char **argv, t_data *data);
+
+// get_files
+
+void			get_files(t_data *data, char **argv);
+
+// utils
+
+void			count_files(t_data *data, char **argv);
 
 #endif
--- cor/main.c
+++ cor/main.c
@@ -3,23 +3,45 @@
 void	create_data(t_data *data)
 {
 	data->index = 1;
+	data->count = 0;
 
 	data->fl.flags = 0;
 	data->fl.n = 0;
 }
+
 
 int		main(int argc, char **argv)
 {
 	t_data data;
 
 	create_data(&data);
-	check_flags(argc, argv, &data);
 
-	if (data.fl.flags > 0)
-	{
-		ft_printf("flags: %i\n", data.fl.flags);
-	}
+//	look for flags and save them to data->fl + add flag "flags";
+	check_flags(argc, argv, &data);
+//	check files for size, min/max number and save them to struct arr data->champs;
+	get_files(&data, argv);
+
+
+	int i;
+
+	i = 0;
+
+
+	off_t	size;
+
+	size = lseek(data.champs[0].code, 0, SEEK_END);
+//	if (size < 0)
+//		error_handler("Error : file is not valid");
+
+	ft_printf("%i\n", size);
+
+//	while (data.champs[0].code[i])
+//	{
+//		ft_printf("%i", (int)data.champs[0].code[i]);
+//		i++;
+//	}
+
+//	ft_printf("%s\n", data.champs[0].code);
 
 	return (0);
-
 }
